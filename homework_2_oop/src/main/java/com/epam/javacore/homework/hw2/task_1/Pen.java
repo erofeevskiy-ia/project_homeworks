a8310151845466fe79ee70b5725c55ba348306d2
Igor <igorer27@yandex.ru>
2017-10-22 22:58:39 +0300

    commit task_1

---
+++ homework_2_oop/src/main/java/com/epam/javacore/homework/hw2/task_1/Pen.java
@@ -0,0 +1,40 @@
+package com.epam.javacore.homework.hw2.task_1;
+
+import java.awt.*;
+
+public class Pen {
+    private Color color;
+
+    public Pen(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pen pen = (Pen) o;
+
+        return color != null ? color.equals(pen.color) : pen.color == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return color != null ? color.hashCode() : 0;
+    }
+
+    /**
+     * FIXME
+     * @return string-describing of currentObject. example: "Color of this pen in RGB-format is[r=12,g=12,b=12]"
+     */
+    @Override
+    public String toString() {
+        Color cur = this.color;
+        return "Color of this pen in RGB-format is "
+                + "[r=" + cur.getRed() + ",g="
+                + cur.getGreen() + ",b=" + cur.getBlue() + "]";
+    }
+
+
+}
