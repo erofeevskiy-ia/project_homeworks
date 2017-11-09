ad392a572b91c4bb5f3597076410f49789d4ed45
Igor <igorer27@yandex.ru>
2017-11-10 00:41:41 +0300

    added find(),getFullLog()

--- homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task_1/CrazyLogger.java
+++ homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task_1/CrazyLogger.java
@@ -4,27 +4,38 @@ package com.epam.javacore.homework.hw3.task_1;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class CrazyLogger  {
-   private StringBuilder crazyLog;
-   private DateTimeFormatter dateTimeFormatter;
+
+public class CrazyLogger {
+    private StringBuilder crazyLog;
+    private DateTimeFormatter dateTimeFormatter;
 
     public CrazyLogger() {
         crazyLog = new StringBuilder();
         dateTimeFormatter = DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm – ");
     }
 
-    public String findInfo(String strInfo){
-       return null;
-   }
+    public String findInfo(String strInfo) {
+        StringBuilder sb = new StringBuilder("Result of searching: ");
+        final int lengthOfStartSB = sb.length();
+        String[] lines = crazyLog.toString().split("\n");
+        for (String s : lines) {
+            if (s.contains(strInfo)) {
+                sb.append(s);
+                sb.append("\n");
+            }
+        }
+        return sb.length() > lengthOfStartSB ? sb.toString() : "searching is unsuccessfully";
+    }
 
-   public void addLogMessage(String message){
-       LocalDateTime localDateTime = LocalDateTime.now();
-       crazyLog.append(localDateTime.format(dateTimeFormatter));
-       crazyLog.append(message);
-       crazyLog.append("\n");
-   }
+    public StringBuilder getCrazyLog() {
+        return crazyLog;
+    }
 
-
-
+    public void addLogMessage(String message) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        crazyLog.append(localDateTime.format(dateTimeFormatter));
+        crazyLog.append(message);
+        crazyLog.append("\n");
+    }
 
 }
