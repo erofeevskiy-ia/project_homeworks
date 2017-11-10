239af3d27fdbbefa7fa18d0c3a8a21ecf2dc6f8b
Igor <igorer27@yandex.ru>
2017-11-10 19:09:10 +0300

    pattern for signature

--- homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task3/FinderRegExp.java
+++ homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task3/FinderRegExp.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FinderRegExp {
-    private String textForAnalyze;
     private final static String ENCODE = "windows-1251";
+    private final static String PATTERN_PICTURE = "[Рр]ис\\.\\s?\\w+[^)]";
+    private final static String PATTERN_PICTURE_FOR_SIGNATURE = "Рис\\.\\s?\\w+((&nbsp;)|(\\s))[А-Я]";
+
+    private String textForAnalyze;
+
     public FinderRegExp(String path){
         try {
             textForAnalyze = readFile(path,ENCODE);
@@ -23,5 +29,15 @@ public class FinderRegExp {
 
     public String getFileString(){
         return textForAnalyze;
+    }
+
+    void find(){
+       // StringBuilder sb=new StringBuilder();
+        Pattern p = Pattern.compile(PATTERN_PICTURE_FOR_SIGNATURE);
+        Matcher m = p.matcher(textForAnalyze);
+        while(m.find()){
+            System.out.println(m.group(0));
+        }
+
     }
 }
