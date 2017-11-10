a5c9e7db3fdb2d3fe5c75144f61477d886e33e37
Igor <igorer27@yandex.ru>
2017-11-10 12:50:50 +0300

    feature for reading file

--- homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task3/FinderRegExp.java
+++ homework_3_string/src/main/java/com.epam.javacore.homework.hw3/task3/FinderRegExp.java
@@ -1,4 +1,27 @@
 package com.epam.javacore.homework.hw3.task3;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FinderRegExp {
+    private String textForAnalyze;
+    private final static String ENCODE = "windows-1251";
+    public FinderRegExp(String path){
+        try {
+            textForAnalyze = readFile(path,ENCODE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String readFile(String path, String encode) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, Charset.forName(encode));
+    }
+
+    public String getFileString(){
+        return textForAnalyze;
+    }
 }
