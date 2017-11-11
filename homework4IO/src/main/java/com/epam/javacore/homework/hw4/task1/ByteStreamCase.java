992feb50956f1aa4a1fa71ec3533be8fa5adaf82
Igor <igorer27@yandex.ru>
2017-11-11 03:55:34 +0300

    task 1 in hw4 done

--- homework4IO/src/main/java/com/epam/javacore/homework/hw4/task1/ByteStreamCase.java
+++ homework4IO/src/main/java/com/epam/javacore/homework/hw4/task1/ByteStreamCase.java
@@ -5,14 +5,14 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ByteStreamCase {
     private static final List<String> KEY_WORDS_JAVA;
+
     static {
-        KEY_WORDS_JAVA= Arrays.asList(
+        KEY_WORDS_JAVA = Arrays.asList(
                 "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
                 "continue", "default", "double", "do", "else", "enum", "extends", "final", "finally",
                 "float", "for", "goto", "if", "implements", "import", "instanceof", "int", "interface",
@@ -22,8 +22,6 @@ public class ByteStreamCase {
     }
 
 
-    //    private FileInputStream fileInputStream;
-//    private FileOutputStream fileOutputStream;
     private String stringInFile;
     private String stringOutFile;
 
@@ -31,43 +29,60 @@ public class ByteStreamCase {
     public ByteStreamCase(String si, String so) {
         stringInFile = si;
         stringOutFile = so;
+        processInputOutput();
+
+    }
+
+    private void processInputOutput() {
         StringBuilder sb = new StringBuilder();
         try (FileInputStream fileInputStream = new FileInputStream(stringInFile);
              FileOutputStream fileOutputStream = new FileOutputStream(stringOutFile)) {
             int c;
-            boolean canIWriteSpace = false;
+            boolean canIWriteItInContainer = false;
             while ((c = fileInputStream.read()) != -1) {
-                if (c > 64 && c < 91 || c > 96 && c < 123) {
+                if (c > 64 && c < 91 || c > 96 && c < 123) { //see ASCII-table
                     while (c > 64 && c < 92 || c > 96 && c < 123) {
-                        sb.append((char) c);
+                        sb.append((char) c);//добавляем посимвольно слово в цикле
                         c = fileInputStream.read();
-                        canIWriteSpace = true;
-
+                        canIWriteItInContainer = true;//метка, что слово кончилось
                     }
 
                 }
-                if (canIWriteSpace){
+                if (canIWriteItInContainer) {
                     sb.append("\n");
                 }
-                canIWriteSpace = false;
+                canIWriteItInContainer = false;
             }
-            System.out.println(sb);
+            String[] lines = sb.toString().split("\n");
+            int sumOfWords = 0;
+            //проверяем, все слова из билдера на принадлежность к ключевым и если так, то сразу записываем в файл
+            for (String s : lines) {
+                if (KEY_WORDS_JAVA.contains(s)) {
+                    for (int i = 0; i < s.length(); i++) {
+                        fileOutputStream.write(s.charAt(i));
+                        if (i == s.length() - 1) {
+                            fileOutputStream.write('\n');
+                            sumOfWords++;
+                        }
+                    }
+                }
+            }
+            String stringNumOFWords = Integer.toString(sumOfWords);
+            for (int i = 0; i < stringNumOFWords.length(); i++) {
+                fileOutputStream.write(stringNumOFWords.charAt(i));
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    void processCount() {
-
-    }
 
     public static void main(String[] args) {
         String so = "C:\\!all\\!epam\\school_Autmn_Lapin\\project_homeworks\\homework4IO\\src\\main\\java\\com\\epam\\javacore\\homework\\hw4\\task1\\out.txt";
         String si = "C:\\!all\\!epam\\school_Autmn_Lapin\\project_homeworks\\homework_2_oop\\src\\main\\java\\com\\epam\\javacore\\homework\\hw2\\task_1\\Pen.java";
-        new ByteStreamCase(so, si);
+        new ByteStreamCase(si, so);
     }
 
 }
